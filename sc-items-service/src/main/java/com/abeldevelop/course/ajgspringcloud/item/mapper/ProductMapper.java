59e8a8ab87b515f7734eb72738e367e97496b4f2
Abel <abeldevelop@users.noreply.github.com>
2020-09-13 00:54:40 +0200

    Section 5: CRUD: consumiendo nuestras APIs con Feign y Rest Template

--- sc-items-service/src/main/java/com/abeldevelop/course/ajgspringcloud/item/mapper/ProductMapper.java
+++ sc-items-service/src/main/java/com/abeldevelop/course/ajgspringcloud/item/mapper/ProductMapper.java
@@ -1,28 +1,27 @@
 package com.abeldevelop.course.ajgspringcloud.item.mapper;
 
-import com.abeldevelop.course.ajgspringcloud.item.api.controller.item.resource.ProductResponseResource;
+import com.abeldevelop.course.ajgspringcloud.item.api.controller.item.resource.ProductResource;
 import com.abeldevelop.course.ajgspringcloud.item.client.productsservice.product.ClientProductResponseResource;
-import com.abeldevelop.course.ajgspringcloud.item.domain.Product;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
 
-  public Product mapClientToDomain(ClientProductResponseResource clientProductResponseResource) {
-    return Product.builder()
+  public ClientProductResponseResource map(ProductResource productResource) {
+    return ClientProductResponseResource.builder()
+        .id(productResource.getId())
+        .name(productResource.getName())
+        .price(productResource.getPrice())
+        .createAt(productResource.getCreateAt())
+        .build();
+  }
+
+  public ProductResource map(ClientProductResponseResource clientProductResponseResource) {
+    return ProductResource.builder()
         .id(clientProductResponseResource.getId())
         .name(clientProductResponseResource.getName())
         .price(clientProductResponseResource.getPrice())
         .createAt(clientProductResponseResource.getCreateAt())
-        .build();
-  }
-
-  public ProductResponseResource mapDomainToResource(Product product) {
-    return ProductResponseResource.builder()
-        .id(product.getId())
-        .name(product.getName())
-        .price(product.getPrice())
-        .createAt(product.getCreateAt())
         .build();
   }
 }
